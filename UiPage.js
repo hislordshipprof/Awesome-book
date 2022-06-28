4d4bc5f450ae1d64c6082eddb0e0baf808886cd8
hislordshipprof <proflord63@gmail.com>
2022-06-28 15:09:55 +0000

    Used Javascript Classes

---
+++ UiPage.js
@@ -0,0 +1,34 @@
+import StoreBook from "./StoreBook.js";
+// class UiPage
+export default class UiPage {
+  // display books
+  static displayBooks() {
+    const books = StoreBook.getBooks();
+
+    books.forEach((book) => {
+      UiPage.addBookToUiPage(book);
+    });
+  }
+
+  // add book to UiPage
+
+  static addBookToUiPage(book) {
+    const container = document.querySelector(".container");
+
+    const ul = document.createElement("ul");
+
+    ul.innerHTML = `
+      <li>${book.title}</li>
+      <li>${book.author}</li>
+      <button class="delete" type="button">Remove</button>
+      <hr />
+    `;
+    container.insertBefore(ul, document.querySelector("#form"));
+  }
+
+  static removeBook(el) {
+    if (el.classList.contains("delete")) {
+      el.parentElement.remove();
+    }
+  }
+}
